34a805fccf92f11d70e8ceee8236fc0b064a8867
ismailpw <80539015+ismailpw@users.noreply.github.com>
2021-07-21 12:17:37 +0200

    Add files via upload

--- test_console_web/test_console.py
+++ test_console_web/test_console.py
@@ -1,4 +1,9 @@
 from readgithubmd import ReadGithubMD
+import os
+import sys
 
-rgm = ReadGithubMD("https://github.com/themorpheustutorials/resources/blob/main/docs/books.md")
-print(rgm.get_mdfile_html())
+rgm = ReadGithubMD("https://github.com/ismailpw/readgithubmd-python/blob/main/README.md")
+with open(os.path.abspath(__file__) + "-test.html", 'w+') as fo:
+    fo.write(rgm.get_mdfile_html())
+    fo.flush()
+    fo.close()
